744951dc5cbae0a6dd5178dc1a77b29db19e0f55
ItsLhun <nahuelghastin@gmail.com>
2021-02-01 08:26:34 +0100

    semi-finished UI, working on funcitonality

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 (function game() {
-  const GameBoard = (() => {
-    let boardArray = [];
-
+  const GameBoard = () => {
+    const boardArray = document.querySelectorAll(".square");
     return { boardArray };
-  })();
+  };
+  (function renderGame(board) {
+    console.log(board);
+    for (let i = 0; i < board.boardArray.length; i++) {
+      board.boardArray[i].textContent = "X";
+    }
+  })(GameBoard());
   const Player = (sign) => {};
 })();
