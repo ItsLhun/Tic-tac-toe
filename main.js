1169baa3fcf2f3d39dafc662d8999a5bfcbafce8
ItsLhun <nahuelghastin@gmail.com>
2021-02-03 08:38:29 +0100

    board display on DOM, working on clickEvent

--- main.js
+++ main.js
@@ -1,13 +1,41 @@
 (function game() {
   const GameBoard = () => {
+    let board = [
+      ["", "O", "O"],
+      ["O", "O", "X"],
+      ["O", "O", "X"],
+    ];
     const boardArray = document.querySelectorAll(".square");
-    return { boardArray };
+    boardArray.forEach((item) => {
+      item.addEventListener("click", () => {
+        console.log(item);
+        board[0][0] = "X";
+      });
+    });
+    return { board, boardArray };
   };
-  (function renderGame(board) {
-    console.log(board);
-    for (let i = 0; i < board.boardArray.length; i++) {
-      board.boardArray[i].firstChild.textContent = "o";
+
+  (function renderGame(gameBoard) {
+    console.log(gameBoard);
+    counter = 0;
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        gameBoard.boardArray[counter].firstChild.textContent =
+          gameBoard.board[i][j];
+        counter++;
+      }
     }
   })(GameBoard());
-  const Player = (sign) => {};
+  const Player = (sign) => {
+    if (sign == "X" || sign == "O") {
+      const playerSign = sign;
+    } else {
+      return console.error("Invalid player sign");
+    }
+    let score = 0;
+    const increaseScore = () => {
+      score++;
+    };
+    return { playerSign, increaseScore };
+  };
 })();
